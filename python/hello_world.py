56e33cff2a4ea24a4b18aa6e3caf01810fdeee68
Laszlo <visualjazz@gmail.com>
2017-01-17 21:02:37 -0600

    Adds tested Hello World in Python from Hacker Rank

---
+++ python/hello_world.py
@@ -0,0 +1,12 @@
+# file: hello_world.py
+# Author: A. Laszlo Lazuer
+# date: 01/17/17
+# Python Version: 2.7
+# description: Exercise in for loops for Python
+
+#Task: Say "Hello, World!" With Python
+#Ask: Print Hello, World!
+
+print 'Hello, World!'
+
+#Passes the sample test case.
